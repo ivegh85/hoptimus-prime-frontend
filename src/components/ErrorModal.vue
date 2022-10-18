690e6ce78167f2834eed1206793106199750322f
LAPTOP-7HUPLMNK\sylvi <wi20b081@technikum-wien.at>
2022-10-18 10:19:00 +0200

    provisorisches error modal

---
+++ src/components/ErrorModal.vue
@@ -0,0 +1,108 @@
+<template>
+  <!--transition funktioniert nicht so ganz-->
+  <!--transition name="fade"-->
+    <div class="vue-modal" v-show="open">
+      <!--transition name="drop-in"-->
+        <div class="vue-modal-inner" v-show="open">
+          <div class="vue-modal-content">
+            <slot />
+            <button type="button" @click="close">close</button>
+          </div>
+        </div>
+      <!--/transition-->
+    </div>
+  <!--/transition-->
+</template>
+
+<script>
+//import { onMounted, onUnmounted} from "vue";
+
+export default {
+  name: "ErrorModal",
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup (_, { emit }) {
+    const close = () => {
+      emit('close')
+    };
+
+    /*verschwindet von selbst------------------
+    const handleKeyup = (event) => {
+      if (event.keyCode === 27){
+        close()
+      }
+    }
+
+    onMounted(() => document.addEventListener("keyup", handleKeyup))
+    onUnmounted(() => document.removeEventListener("keyup", handleKeyup))
+    ---------------------------------------------*/
+
+    return { close };
+  }
+};
+</script>
+
+<style scoped>
+
+*,
+::before,
+::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.vue-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  background-color: rgba(0,0,0,0.4);
+  z-index: 1;
+}
+
+.vue-modal-inner {
+  max-width: 500px;
+  margin: 2rem auto;
+}
+
+.vue-modal-content {
+  position: relative;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.3);
+  background-clip: padding-box;
+  border-radius: 0.3rem;
+  padding: 1rem
+}
+
+/* haut nicht so ganz hin
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.drop-in-enter-active,
+.drop-in-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.drop-in-enter-from,
+.drop-in-leave-to {
+  opacity: 0;
+  transform: translateY(-50px);
+}
+
+ */
+</style>
