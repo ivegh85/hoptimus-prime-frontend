e786da6fe895792841e5f3439d4716f014980e7f
LAPTOP-7HUPLMNK\sylvi <wi20b081@technikum-wien.at>
2022-10-18 10:17:15 +0200

    provisorisches error modal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <Navigation></Navigation>
   <router-view/>
+  <Auctions></Auctions>
+
+  <button @click="isOpen = true">Show Modal</button>
+
+  <ErrorModal :open="isOpen" @close="isOpen = !isOpen">
+    <p>
+      An error occurred
+    </p>
+  </ErrorModal>
+
 </template>
 
 <script>
+import { ref } from "vue";
+
 import NavigationBar from "@/components/NavigationBar";
+import Auctions from "@/views/auction/Auctions";
+import ErrorModal from "@/components/ErrorModal";
+
 
 
 export default {
   name: 'App',
   components: {
     'Navigation': NavigationBar,
+    'Auctions': Auctions,
+    'ErrorModal': ErrorModal
+  },
+  setup(){
+    const isOpen = ref(false)
+    return { isOpen }
   }
 }
 </script>
